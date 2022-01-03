d44c91711706247d5f6c8ea2c6ea8d5870a89e9f
Soufiane ait fakir ali <aitfakiralisoufiane.1998@gmail.com>
2022-01-03 16:24:45 +0100

    commit

--- components/Nav.js
+++ components/Nav.js
@@ -1,30 +1,33 @@
-
-import { useRouter } from 'next/router';
-import requests from '../utils/requests';
+import { useRouter } from "next/router";
+import requests from "../utils/requests";
 
 const Nav = () => {
-    const router = useRouter();
-    return (
-       <nav className="relative">
-           <div className="flex px-10 sm:px-20
+  const router = useRouter();
+  return (
+    <nav className="relative">
+      <div
+        className="flex px-10 sm:px-20
            whitespace-nowrap space-x-10
-           sm:space-x-20 overflow-x-scroll scrollbar-hide">
-                {Object.entries(requests).map(([key,{title,url}]) => {
-                    return <h2
-                    key={key}
-                    onClick={() => router.push(`?genre=${key}`)}
-                    className="cursor-pointer transition duration-199
+           sm:space-x-20 overflow-x-scroll scrollbar-hide"
+      >
+        {Object.entries(requests).map(([key, { title, url }]) => {
+          return (
+            <h2
+              key={key}
+              onClick={() => router.push(`?genre=${key}`)}
+              className="cursor-pointer transition duration-199
                     transform hover:scale-125 hover:text-white
                     active:text-red-500"
-                    >
-                        {title}
-                    </h2>
-                })}
-           </div>
-           <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" ></div>
-           <div className="absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12" ></div>
-       </nav>
-    )
-}
+            >
+              {title}
+            </h2>
+          );
+        })}
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
+      <div className="absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12"></div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
